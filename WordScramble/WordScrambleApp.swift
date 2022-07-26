a5ef71b25a9dc89a6796246047e79d711861d4e9
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-07-26 15:21:22 +0530

    initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Admin on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
